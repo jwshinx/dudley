b124ad679f960333061a8b84095e8d8fcf43884b
Joel Shin <jwshin@gmail.com>
2012-02-19 20:32:13 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosemary::Application.config.secret_token = '629f33ae1f34fb363433a56b01287d867ed76a75a42d127e7d2f074f16f5fa3f086631a61efa80b0984fbc3d016b1c05f7e46146362d29d6fb3351655cd7787f'
